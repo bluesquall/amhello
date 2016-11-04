d63a8926f6f9b587648b386e74c7f7dba8d16870
M J Stanway <m.j.stanway@alum.mit.edu>
2016-11-03 20:57:24 -0700

    add source for hello program

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <config.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    puts ("Hello, cuel world!");
+    puts ("This is " PACKAGE_STRING ".");
+    return EXIT_SUCCESS;
+}
