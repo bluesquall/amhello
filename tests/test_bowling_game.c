5222fa7ee05cfca78ffb15634d293691ee573279
M J Stanway <m.j.stanway@alum.mit.edu>
2016-11-04 16:59:07 -0700

    add test for bowling_game

---
+++ tests/test_bowling_game.c
@@ -0,0 +1,12 @@
+// test_bowling_game.c
+// A test-driven development example
+// see http://www.slideshare.net/amritayan/test-driven-development-in-c
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    assert( false && "my first unit test" );
+}
