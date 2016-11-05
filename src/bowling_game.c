f5354d1c51aa8458dae6d673f3facc3b541706e6
M J Stanway <m.j.stanway@alum.mit.edu>
2016-11-04 17:38:29 -0700

    add empty implementation of bowling game

---
+++ src/bowling_game.c
@@ -0,0 +1,16 @@
+// bowling_game.c
+// A test-driven development example
+// see http://www.slideshare.net/amritayan/test-driven-development-in-c
+
+#include "bowling_game.h"
+
+void bowling_game_init (void)
+{}
+
+void bowling_game_roll (int pins)
+{}
+
+int bowling_game_score (void)
+{
+    return -1;
+}
