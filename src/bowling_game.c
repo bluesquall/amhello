787075f38c01515a39f9ad752fb80f991e1f17ba
M J Stanway <m.j.stanway@alum.mit.edu>
2016-11-04 18:29:39 -0700

    now roll actually tracks rolls, and score actually calculates the score

--- src/bowling_game.c
+++ src/bowling_game.c
@@ -4,19 +4,26 @@
 
 #include "bowling_game.h"
 
-static int score = 0;
+enum { max_rolls = 21 };
+static int roll[max_rolls] = { 0 };
+static int current_roll = 0;
 
 void bowling_game_init (void)
 {
-    score = 0;
+    for (int i=0; i<max_rolls; i++)
+        roll[i] = 0;
+    current_roll = 0;
 }
 
 void bowling_game_roll (int pins)
 {
-    score += pins;
+    roll[current_roll++] = pins;
 }
 
 int bowling_game_score (void)
 {
+    int score = 0;
+    for (int i=0; i<max_rolls; i++)
+        score += roll[i];
     return score;
 }
