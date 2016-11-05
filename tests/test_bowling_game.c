f5e69a283a3c936bdfc55810c010973755406c85
M J Stanway <m.j.stanway@alum.mit.edu>
2016-11-04 17:05:55 -0700

    write first real test (gutter game)

--- tests/test_bowling_game.c
+++ tests/test_bowling_game.c
@@ -6,7 +6,15 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+static void test_gutter_game (void)
+{
+    bowling_game_init();
+    for (int i=0; i<20; i++)
+        bowling_game_roll(0);
+    assert( bowling_game_score() == 0 && "test_gutter_game()" );
+}
+
 int main (void)
 {
-    assert( false && "my first unit test" );
+    test_gutter_game();
 }
